bf453a76f227e94fb275c67cee13560653f528c3
Umarrrrrrrrrrrgs <fernandobriggs71@gmail.com>
2024-06-29 08:31:20 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,8 +22,8 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['Umar','Umar','Umar'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['Umar'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
 global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
